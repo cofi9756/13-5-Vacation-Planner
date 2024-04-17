6d81aae01e8b6ad77c956eee5e78252c3f034d22
Thomas Garcia <thga6708@colorado.edu>
2024-04-17 17:48:44 -0600

    add gitignore

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -5,23 +5,48 @@ CREATE TABLE users (
     email VARCHAR(100) UNIQUE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    date_of_birth date 
+    date_of_birth date
 );
 
-CREATE TABLE locations (
-    locationid SERIAL PRIMARY KEY NOT NULL,
-    location_name VARCHAR(100) NOT NULL,
+CREATE TABLE countries (
+    countryid SERIAL PRIMARY KEY NOT NULL,
+    country_name VARCHAR(100) NOT NULL,
+    country_desc VARCHAR(150) NOT NULL,
     preference_data VARCHAR(150)
 );
 
+CREATE TABLE cities (
+    cityid SERIAL PRIMARY KEY NOT NULL,
+    countryid INT NOT NULL,
+    city_name VARCHAR(100) NOT NULL,
+    city_desc VARCHAR(150) NOT NULL,
+    preference_data VARCHAR(150),
+    FOREIGN KEY (countryid) REFERENCES countries(countryid)
+);
+
+-- CREATE TABLE locations (
+--     locationid SERIAL PRIMARY KEY NOT NULL,
+--     location_name VARCHAR(100) NOT NULL,
+--     preference_data VARCHAR(150)
+-- );
+
 CREATE TABLE events (
     eventid SERIAL PRIMARY KEY NOT NULL,
-    locationid INT NOT NULL,
+    countryid INT NOT NULL,
     event_name VARCHAR(100) NOT NULL,
+    event_desc VARCHAR(150),
     event_date date NOT NULL,
     event_time time NOT NULL,
     preference_data VARCHAR(150),
-    FOREIGN KEY (locationid) REFERENCES locations(locationid)
+    FOREIGN KEY (countryid) REFERENCES countries(countryid)
+);
+
+CREATE TABLE images (
+    imageid SERIAL PRIMARY KEY NOT NULL,
+    eventid INT NOT NULL,
+    image_link VARCHAR(150),
+    image_desc VARCHAR(100),
+    FOREIGN KEY (eventid) REFERENCES events(eventid)
 );
 
 CREATE TABLE saved_events (
